9c8e0d597c6d2eec7203f7191b70f65403b878e7
sangameshwarp7000 <136581267+sangameshwarp7000@users.noreply.github.com>
2024-07-05 11:12:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -20,5 +20,5 @@ mongoose.connect(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: t
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.log(err));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
